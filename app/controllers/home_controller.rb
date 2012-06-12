d01b0726d85165736ef004c8f4d5fca029c30687
Rahul Mitra <rahulmitra.az@gmail.com>
2012-06-12 14:57:03 -0700

    final

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,22 @@ class HomeController < ApplicationController
   
   def index
     @title = "Manzanar Home Page"
-  end  
+  end 
+  
+  def sources
+    
+  end 
+  
+  def traditions_food
+    
+  end
+  
+  def traditions_sandals
+    
+  end
+  
+  def events
+    
+  end
   
 end
